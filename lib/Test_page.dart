5df337c2ba968fbef902e34d7424fdcdea19c01b
DoLLaRs6252 <punvarattaya6252@gmail.com>
2024-01-27 09:07:12 +0700

    first commit

---
+++ lib/Test_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Test_page extends StatelessWidget {
+  const Test_page({super.key});
+
+   Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("hello world"),
+      )
+    )    
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'midterm',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+        scaffoldBackgroundColor: Color.fromARGB(255, 255, 255, 255),
+      ),
+      home: const Test_page(),
+    );
+  }
+}
+
